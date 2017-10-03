02db8de5c8d4cae34a9fb5f817fb0144d667cfb1
Laura Trager <laura.trager@gmail.com>
2017-10-03 16:09:57 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def position_taken?(array, index)
+  if array[index] == "" || array[index] == " " || array[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(array, index)
+  if position_taken?(array, index) == false && index.between?(0, 8)
+    true
+  else
+    false
+  end
+end
+
+def move(array, index, char = 'X')
+  array[index] = char
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(array, index) == true && position_taken?(array, index) == false
+    move(array, index, char = 'X')
+  else valid_move?(array, index) == false || position_taken?(array, index) == true
+    puts "Please enter a valid number, or the number for an unoccupied space"
+    turn(board)
+  end
+end
